0e4881b6a49be70e5e0a965b6a53c2cfd469fb45
Ihar Novikau <gosha.noviko2012@gmail.com>
2022-09-05 17:15:53 +0300

    feat: start project

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ const baseConfig = {
                 test: /\.(jpg|png|svg|gif)$/,
                 type: 'asset/resource',
             },
+            {test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader'}
         ],
     },
     resolve: {
