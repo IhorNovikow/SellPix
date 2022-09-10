258d28b56ef96dd887ee710df16c3d00b78311bd
Ihar Novikau <gosha.noviko2012@gmail.com>
2022-09-10 18:41:25 +0300

    feat: crete some section main

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 import './assets/fonts/Gilroy/stylesheet.css';
 import './assets/fonts/BebasNeus/stylesheet.css';
+
+import './style/html/html.css';
+import './style/html/body.css';
+import './style/html/reset.css';
+
+import './style/header/header.css';
+import './style/header/nav.css';
+
+import './style/main/main.css';
+import './style/main/sectionBanerMain.css';
+import './style/main/sectionGameMain.css';
+import './style/main/mainSectionConfidence.css';
+import './style/main/sectionBigDiscont.css';
+import './style/main/sectionReviews.css';
+
+import './style/footer/footer.css';
